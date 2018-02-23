522ce4189f6c86bd5663387675f5818aab808571
busyxin <xin@busyxin.com>
2018-02-23 02:06:54 +0100

    Solve merge conflicts

--- main.js
+++ main.js
@@ -1,182 +1,156 @@
-window.onload = function () { 
-    
-    // PAINTING THE GRAPH = g
+var svg = d3.select("svg"),
+    margin  = {top: 20, right: 20, bottom: 110, left: 40},
+    margin2 = {top: 430, right: 20, bottom: 30, left: 40},
+    width   = +svg.attr("width") - margin.left - margin.right,
+    height  = +svg.attr("height") - margin.top - margin.bottom,
+    height2 = +svg.attr("height") - margin2.top - margin2.bottom;
 
-    var margin = {top: 20, right: 20, bottom: 30, left: 50};
-    
-    
-    var svg = d3.select("svg");
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
+var parseDate = d3.timeParse("%-d/%-m/%Y");
+var parseTime = d3.timeParse("%d/%m/%Y");
 
+var x  = d3.scaleTime().range([0, width]),
+    x2 = d3.scaleTime().range([0, width]),
+    y  = d3.scaleLinear().range([height, 0]),
+    y2 = d3.scaleLinear().range([height2, 0]);
 
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var xAxis  = d3.axisBottom(x),
+    xAxis2 = d3.axisBottom(x2),
+    yAxis  = d3.axisLeft(y);
 
+var brush = d3.brushX()
+              .extent([[0, 0], [width, height2]])
+              .on("brush end", brushed);
 
+var zoom = d3.zoom()
+             .scaleExtent([1, Infinity])
+             .translateExtent([[0, 0], [width, height]])
+             .extent([[0, 0], [width, height]])
+             .on("zoom", zoomed);
 
-    //  Create time parsers
-    var parseTime = d3.timeParse("%d/%m/%Y");
-    var parseTime2 = d3.timeParse("%Y-%m-%d");
+var line = d3.line()
+             .x(function(d) { return x(d.date); })
+             .y(function(d) { return y(d.close); });
 
+var line2 = d3.line()
+              .x(function(d) { return x2(d.date); })
+              .y(function(d) { return y2(d.close); });
 
-    // Set scales, 
-    // x is a scale for time
-    var x = d3.scaleTime()
-        .range([0, width]);
+svg.append("defs").append("clipPath")
+   .attr("id", "clip")
+   .append("rect")
+   .attr("width", width)
+   .attr("height", height);
 
-    // y is a scale mapping value to height
-    var y = d3.scaleLinear()
-        .range([height, 0]);
+var focus = svg.append("g")
+               .attr("class", "focus")
+               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // the line maps x from date and y from closing value
-    var line = d3.line()
-        .x(function(d) { return x(d.Date); })
-        .y(function(d) {
-          return y(d.Close); });
+var context = svg.append("g")
+                 .attr("class", "context")
+                 .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
+d3.queue()
+  .defer(d3.csv, "bitcoin.csv", type)
+  // .defer(d3.csv, "ethereum.csv", type)
+  // .defer(d3.csv, "iota.csv", type)
+  // .defer(d3.csv, "nem.csv", type)
+  // .defer(d3.csv, "ripple.csv", type)
+  // .defer(d3.csv, "tether.csv", type)
+  // .defer(d3.csv, "vrc.csv", type)
+  // .defer(d3.csv, "bitcoinAttacks.csv", type)
+  .await(function(error, bitcoinData) {
+      if (error) {
+        console.error('Oh mein Gott! Something went terribly wrong: ' + error);
 
-d3.csv("bitcoin.csv", function(error1, data1) {                   //  Load bitcoin values from csv
-   d3.csv("BitcoinHacksInDollars.csv", function(error2, data2) {  //  Load hack values from csv
-    
-    //  Filter bitcoin values
-    dataset = data1.map(function(d) { return { Date: parseTime(d.Date), Close: +d["Close"] } });  
-    // Filter hacks values
-    dataset2 = data2.map(function(d) {             
-      return {
-        Date: parseTime2(d.Date),
-        Loss: +d["Loss in $"],
-        Website: d["Source"],
-        TypeOfFraud: d["Fraud/Hack"],
-        Platform: d["Platform/Service"],
-        lossInBTC: d["Loss in BTC"]
+      } else {
+        renderCharts([bitcoinData]);
       }
-    });
-    
-    //  Tooptip stuff
-    var tip = d3.select('body')
-      .append('div')
-      .attr('class', 'tip')
-      .html('I am a tooltip...')
-      .style('border', '1px solid steelblue')
-      .style("border-radius","10px")
-      .style('padding', '5px')
-      .style('background-color', '5px')
-      .style('position', 'absolute')
-      .style('display', 'none')
-      .on('mouseover', function(d, i) {
-        tip.transition().duration(0);
-      })
-      .on('mouseout', function(d, i) {
-        tip.style('display', 'none');
-      });
+  });
 
+function renderLine(target, data, color, secondary) {
+  target.append("path")
+    .datum(data)
+    .attr("class", "line")
+    .attr("d", line)
+    .attr("fill", "none")
+    .attr("stroke", color)
+    .attr("opacity", "0")
+    .transition()
+    .attr("opacity", "1")
+    .duration(1000)
+    .delay(1000)
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .attr("stroke-width", 1.5);
+}
 
+function renderLine2(target, data, color) {
+  renderLine(target, data, color, true);
+}
 
-    // Scale the range of the data
-    x.domain(d3.extent(dataset, function(d) { return d.Date; }));
-    console.log(x.domain());
-    y.domain(d3.extent(dataset, function(d) { return d.Close; }));
+function renderCharts(cryptoArray, attackArray) {
+  x.domain(d3.extent(cryptoArray[0], function(d) { return d.date; }));
+  y.domain(d3.extent(cryptoArray[0], function(d) { return d.close; }));
 
-    // filter out hacks that are outside of our timeline
-    var startDate = x.domain()[0].getTime();
-    var endDate = x.domain()[1].getTime();
-    dataset2 = dataset2.filter(o => (o.Date && o.Date.getTime() <= endDate && o.Date.getTime() > startDate));
+  x2.domain(x.domain());
+  y2.domain(y.domain());
 
-    for(var i = 0; i < dataset2.length; i++){
-      d = dataset2[i];
-      d.Close = dataset.filter(o => o.Date.getTime() == d.Date.getTime()).map(o => o.Close)[0];
-    }
+  cryptoArray.map(function(cryto) { 
+    var dataset = cryto.map(function(d) { return { date: parseTime(d.date), close: +d.close } });  
+    renderLine(focus, dataset, "blue"); 
+  });
 
-    //  Axis
-    g.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+  focus.append("g")
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
-    g.append("g")
-      .call(d3.axisLeft(y))
-      .append("text")
-      .attr("fill", "#000")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Price ($)");
+  focus.append("g")
+    .attr("class", "axis axis--y")
+    .call(yAxis);
 
-    //  Path
-    g.append("path")
-      .datum(dataset) //  Use dataset that holds bitcoin closing values for line chart
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("stroke-width", 1.5)
-      .attr("d", function(d) {
-        return line(d); });
+  cryptoArray.map(function(crypto) { renderLine(context, crypto, "red", true); });
 
-    // Add the scatterplot
-   svg.selectAll("dot")
-      .data(dataset2)                     //  Use dataset2 that holds all the hacks for scatterplot
-      .enter().append("circle")
-      .attr("cx", function(d) { return x(d.Date); })    //  use date coordiantes for x axis
-      .attr("cy", function(d) { return y(d.Close); })
-      .style("fill", function(d) {        //  Diffrent colors for diffrent hacks
-        if (d.TypeOfFraud == "Hack") {
-          return "Green";
-        } else if (d.TypeOfFraud == "Scam") {
-          return "Purple";
-        } else {
-          return "Red"
-        }
-      })
-      .attr("r", function(d) {          //  Diffrent size of circles for diffrent amount of loss
-        if (d.Loss > 250000 && d.Loss < 1000000) {
-          return 10;
-        } else if (d.Loss > 10000000 && d.Loss < 50000000) {
-          return 15;
-        } else if (d.Loss > 50000000) {
-          return 20;
-        } else {
-          return 5;
-        }
-      })
-      .on("click", function(d) {      //  On click go to the website that describes hack
-        window.open(d["Website"]); 
-      })
-      .on('mouseover', function(d, i) {   //  Mouseover
-        tip.transition().duration(0);
-          //  Get coordinates of mouse for tooltip positioning
-        var coordinates = [0, 0];
-        coordinates = d3.mouse(this); 
-        var x = coordinates[0];
-        var y = coordinates[1];
-        //  Position tooltip
-        tip.style("left", x + 25 + "px")
-        tip.style("top", y - 140 + "px")
-        tip.style('display', 'block');
-        //  ES6 TEMPLATE string, this is the values given to tooltip
-        var html = `               
-        <h4>Click on circle to go to news article </h4>
-          <ul>    
-            <li>Platform of hack: ${d.Platform} </li>
-            <li>Date of hack: ${d.Date} </li>
-            <li>Loss in dollars: ${d.Loss} </li>
-            <li>Loss in BTC: ${d.lossInBTC} </li>
-            <li>Type of hack: ${d.TypeOfFraud} </li>
-            <li>Closing price of this day: ${d.Close} </li>
-          </ul>
+  context.append("g")
+    .attr("class", "axis axis--x")
+    .attr("transform", "translate(0," + height2 + ")")
+    .call(xAxis2);
 
-        `;
-        tip.html(html);     //  Give our template string to the tooltip for output
-      })
-      .on('mouseout', function(d, i) {  //  Mouseout
-        tip.transition()
-        .delay(800)
-        .style('display', 'none');
-      });
+  context.append("g")
+    .attr("class", "brush")
+    .call(brush)
+    .call(brush.move, x.range());
 
+  svg.append("rect")
+    .attr("class", "zoom")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .call(zoom);
+}
 
+function brushed() {
+  if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return;
+  var s = d3.event.selection || x2.range();
+  x.domain(s.map(x2.invert, x2));
+  focus.select(".line").attr("d", line);
+  focus.select(".axis--x").call(xAxis);
+  svg.select(".zoom").call(zoom.transform, d3.zoomIdentity
+    .scale(width / (s[1] - s[0]))
+    .translate(-s[0], 0));
+}
 
-   });
- });
+function zoomed() {
+  if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return;
+  var t = d3.event.transform;
+  x.domain(t.rescaleX(x2).domain());
+  focus.select(".line").attr("d", line);
+  focus.select(".axis--x").call(xAxis);
+  context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
+}
 
- }
+function type(d) {
+  d.date = parseDate(d.date);
+  d.close = +d.close;
+  return d;
+}
---
+++ test/main.js
@@ -0,0 +1,182 @@
+window.onload = function () { 
+    
+    // PAINTING THE GRAPH = g
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 50};
+    
+    
+    var svg = d3.select("svg");
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
+
+
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+    //  Create time parsers
+    var parseTime = d3.timeParse("%d/%m/%Y");
+    var parseTime2 = d3.timeParse("%Y-%m-%d");
+
+
+    // Set scales, 
+    // x is a scale for time
+    var x = d3.scaleTime()
+        .range([0, width]);
+
+    // y is a scale mapping value to height
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+
+    // the line maps x from date and y from closing value
+    var line = d3.line()
+        .x(function(d) { return x(d.Date); })
+        .y(function(d) {
+          return y(d.Close); });
+
+
+d3.csv("bitcoin.csv", function(error1, data1) {                   //  Load bitcoin values from csv
+   d3.csv("BitcoinHacksInDollars.csv", function(error2, data2) {  //  Load hack values from csv
+    
+    //  Filter bitcoin values
+    dataset = data1.map(function(d) { return { Date: parseTime(d.Date), Close: +d["Close"] } });  
+    // Filter hacks values
+    dataset2 = data2.map(function(d) {             
+      return {
+        Date: parseTime2(d.Date),
+        Loss: +d["Loss in $"],
+        Website: d["Source"],
+        TypeOfFraud: d["Fraud/Hack"],
+        Platform: d["Platform/Service"],
+        lossInBTC: d["Loss in BTC"]
+      }
+    });
+    
+    //  Tooptip stuff
+    var tip = d3.select('body')
+      .append('div')
+      .attr('class', 'tip')
+      .html('I am a tooltip...')
+      .style('border', '1px solid steelblue')
+      .style("border-radius","10px")
+      .style('padding', '5px')
+      .style('background-color', '5px')
+      .style('position', 'absolute')
+      .style('display', 'none')
+      .on('mouseover', function(d, i) {
+        tip.transition().duration(0);
+      })
+      .on('mouseout', function(d, i) {
+        tip.style('display', 'none');
+      });
+
+
+
+    // Scale the range of the data
+    x.domain(d3.extent(dataset, function(d) { return d.Date; }));
+    console.log(x.domain());
+    y.domain(d3.extent(dataset, function(d) { return d.Close; }));
+
+    // filter out hacks that are outside of our timeline
+    var startDate = x.domain()[0].getTime();
+    var endDate = x.domain()[1].getTime();
+    dataset2 = dataset2.filter(o => (o.Date && o.Date.getTime() <= endDate && o.Date.getTime() > startDate));
+
+    for(var i = 0; i < dataset2.length; i++){
+      d = dataset2[i];
+      d.Close = dataset.filter(o => o.Date.getTime() == d.Date.getTime()).map(o => o.Close)[0];
+    }
+
+    //  Axis
+    g.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    g.append("g")
+      .call(d3.axisLeft(y))
+      .append("text")
+      .attr("fill", "#000")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Price ($)");
+
+    //  Path
+    g.append("path")
+      .datum(dataset) //  Use dataset that holds bitcoin closing values for line chart
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", function(d) {
+        return line(d); });
+
+    // Add the scatterplot
+   svg.selectAll("dot")
+      .data(dataset2)                     //  Use dataset2 that holds all the hacks for scatterplot
+      .enter().append("circle")
+      .attr("cx", function(d) { return x(d.Date); })    //  use date coordiantes for x axis
+      .attr("cy", function(d) { return y(d.Close); })
+      .style("fill", function(d) {        //  Diffrent colors for diffrent hacks
+        if (d.TypeOfFraud == "Hack") {
+          return "Green";
+        } else if (d.TypeOfFraud == "Scam") {
+          return "Purple";
+        } else {
+          return "Red"
+        }
+      })
+      .attr("r", function(d) {          //  Diffrent size of circles for diffrent amount of loss
+        if (d.Loss > 250000 && d.Loss < 1000000) {
+          return 10;
+        } else if (d.Loss > 10000000 && d.Loss < 50000000) {
+          return 15;
+        } else if (d.Loss > 50000000) {
+          return 20;
+        } else {
+          return 5;
+        }
+      })
+      .on("click", function(d) {      //  On click go to the website that describes hack
+        window.open(d["Website"]); 
+      })
+      .on('mouseover', function(d, i) {   //  Mouseover
+        tip.transition().duration(0);
+          //  Get coordinates of mouse for tooltip positioning
+        var coordinates = [0, 0];
+        coordinates = d3.mouse(this); 
+        var x = coordinates[0];
+        var y = coordinates[1];
+        //  Position tooltip
+        tip.style("left", x + 25 + "px")
+        tip.style("top", y - 140 + "px")
+        tip.style('display', 'block');
+        //  ES6 TEMPLATE string, this is the values given to tooltip
+        var html = `               
+        <h4>Click on circle to go to news article </h4>
+          <ul>    
+            <li>Platform of hack: ${d.Platform} </li>
+            <li>Date of hack: ${d.Date} </li>
+            <li>Loss in dollars: ${d.Loss} </li>
+            <li>Loss in BTC: ${d.lossInBTC} </li>
+            <li>Type of hack: ${d.TypeOfFraud} </li>
+            <li>Closing price of this day: ${d.Close} </li>
+          </ul>
+
+        `;
+        tip.html(html);     //  Give our template string to the tooltip for output
+      })
+      .on('mouseout', function(d, i) {  //  Mouseout
+        tip.transition()
+        .delay(800)
+        .style('display', 'none');
+      });
+
+
+
+   });
+ });
+
+ }
