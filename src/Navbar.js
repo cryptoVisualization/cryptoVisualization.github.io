f751083c2bd55fba11aa8f7e83ef6a6f281612c6
busyxin <xin@busyxin.com>
2018-03-05 15:13:31 +0100

    Add font-face into index.css

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
   render() {
     return (
       <ul className="navbar">
-        <li className="navbar__item"><NavLink className="navbar__link" to="/"></NavLink></li>
+        <li className="navbar__menu"><NavLink className="navbar__link" to="/"><span></span><span></span><span></span></NavLink></li>
         <li className="navbar__item"><NavLink className="navbar__link" to="/introduction">01</NavLink></li>
         <li className="navbar__item"><NavLink className="navbar__link" to="/cryptocurrencies">02</NavLink></li>
         <li className="navbar__item"><NavLink className="navbar__link" to="/attacks">03</NavLink></li>
