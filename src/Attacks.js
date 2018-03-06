e822b55c47d2594eaed2fee61d04e54217e766b9
busyxin <xin@busyxin.com>
2018-03-06 15:48:25 +0100

    Add content, remove attack

--- src/Attacks.js
+++ src/Attacks.js
@@ -14,8 +14,18 @@ class Attacks extends Component {
       <div className="section-container section-container--dark">
         <section className="section">
           <h2>What are the different kind of attacks?</h2>
-          <p>A cryptocurrency is a digital currency based around the idea of using cryptography to allow for decentralization of the control of the currency. This works through the use of so called blockchains, which are essentially public digital ledgers holding a list of blocks that typically contain a cryptographic hash of the prior block in the blockchain along with transaction data and a timestamp. The blockchains work on the basis of proof-of-work schemes that use hashing algorithms to prove work done, as they can be used to create tasks that are demanding to solve but it is trivial to check whether a solution is correct. This proof of work allows for the protocol to be secure by design by always favoring a blockchain that has more work done as the valid blockchain.</p>
-          <p>The result is a system that does not require a central authority or trust between users as it is infeasible to fake a transaction because of the system being authenticated by mass collaboration based on collective self interest. A blockchain containing a fake transaction would eventually end up being replaced by the valid blockchain once more work has been done in the valid blockchain, which means that in order to theoretically fake transactions you would need to be in control of a majority of the processing power within the system.</p>
+          <div className="attack-block">
+            <h3>Hack:</h3>
+            <p>A hack is an attack abusing a software vulnerability allowing an attacker to get access to data they should not be able to access making it possible to for example make transactions using other people’s funds. This is the most common type of attack that we analyze in our visualization.</p>
+          </div>
+          <div className="attack-block">
+            <h3>Scam:</h3>
+            <p>A scam is an attack where the attacker gains access to funds through manipulation of people rather than accessing the funds directly, examples of this often make use of tactics such as identity theft or fraudulent services posing as legitimate in order to steal.</p>
+          </div>
+          <div className="attack-block">
+            <h3>Ponzi Scheme:</h3>
+            <p>Some cryptocurrency attacks are based around having an exchange service or even an entire currency function as a ponzi scheme, where earlier adopters of the service fraudulently profit from newer users investing in the service.</p> 
+          </div>
         </section>
       </div>
     );
--- src/Navbar.js
+++ src/Navbar.js
@@ -13,9 +13,9 @@ class Navbar extends Component {
         <li className="navbar__item"><NavLink className="navbar__link" to="/cryptocurrencies">02</NavLink></li>
         <li className="navbar__item"><NavLink className="navbar__link" to="/attacks">03</NavLink></li>
         <li className="navbar__item"><NavLink className="navbar__link" to="/visualization">04</NavLink></li>
-        <li className="navbar__item"><NavLink className="navbar__link" to="/conclusions">05</NavLink></li>
-        <li className="navbar__item"><NavLink className="navbar__link" to="/references">06</NavLink></li>
-        <li className="navbar__item"><NavLink className="navbar__link" to="/aboutus">07</NavLink></li>
+        {/* <li className="navbar__item"><NavLink className="navbar__link" to="/conclusions">05</NavLink></li>
+        <li className="navbar__item"><NavLink className="navbar__link" to="/references">06</NavLink></li> */}
+        <li className="navbar__item"><NavLink className="navbar__link" to="/aboutus">05</NavLink></li>
       </ul>
     );
   }
